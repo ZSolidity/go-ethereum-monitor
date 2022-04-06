8bc8c3398933a91e8b2d4f04836d23500dc65a29
DESKTOP-IVRHEJ7\JiZai <719896508@qq.com>
2022-04-06 12:31:20 +0800

    fix topics

--- pkg/config.go
+++ pkg/config.go
@@ -9,22 +9,22 @@ type config struct {
 
 func NewConfig() *config {
 	return &config{
-		addresses: make([]common.Address,0),
-		topics:    make([][]common.Hash,0),
+		addresses: make([]common.Address, 0),
+		topics:    make([][]common.Hash, 0),
 	}
 }
-func (c *config) AddAddressWithAddress(address common.Address)  {
-	c.addresses = append(c.addresses,address)
+func (c *config) AddAddressWithAddress(address common.Address) {
+	c.addresses = append(c.addresses, address)
 }
 
-func (c *config) AddAddressWithString(address string)  {
-	c.addresses = append(c.addresses,common.HexToAddress(address))
+func (c *config) AddAddressWithString(address string) {
+	c.addresses = append(c.addresses, common.HexToAddress(address))
 }
 
-func (c *config) AddTopicWithHash(topic common.Hash)  {
-	c.topics = append(c.topics,[]common.Hash{topic})
+func (c *config) AddTopicWithHash(topic common.Hash) {
+	c.topics[0] = append(c.topics[0], topic)
 }
 
-func (c *config) AddTopicWithString(topic string)  {
-	c.topics = append(c.topics,[]common.Hash{common.HexToHash(topic)})
+func (c *config) AddTopicWithString(topic string) {
+	c.topics[0] = append(c.topics[0], common.HexToHash(topic))
 }
